0d146a2ca849add533d312100fdb2c0a37326ca1
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-06-01 22:15:58 +0530

    Updated API URL

--- src/service/services.jsx
+++ src/service/services.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
 			// baseURL: "https://doubt-overflow-api.herokuapp.com/api/v1",
-			baseURL: "http://localhost:8000/api/v1",
-			// baseURL: "https://track-it-v2.herokuapp.com/api/v2",
+			// baseURL: "http://localhost:8000/api/v1",
+			baseURL: "https://gcrf.herokuapp.com/api/v1",
 		});
 
 export const getReport = (email) => api.get(`/${email}`);
