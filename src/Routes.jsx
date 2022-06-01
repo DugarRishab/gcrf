8b05e09b596be06c1378c0a2193972ab1a1ceb15
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-06-01 22:21:30 +0530

    Updated routing

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home/Home';
-import Home2 from './pages/Home/Home2';
-import LeaderBoard from './pages/Leaderboard/Leaderboard';
-import Report from './pages/Report/Report';
+import Home from "./pages/Home/Home";
+import Home2 from "./pages/Home/Home2";
+import LeaderBoard from "./pages/Leaderboard/Leaderboard";
+import Report from "./pages/Report/Report";
 
 const AllRoutes = () => {
 	return (
@@ -16,10 +16,22 @@ const AllRoutes = () => {
 					<Home2></Home2>
 				}
 			></Route>
+			<Route
+				exact
+				path="/"
+				element={
+					// <Home></Home>
+					<Home2></Home2>
+				}
+			></Route>
 			<Route exact path="/report" element={<Report></Report>}></Route>
-			<Route exact path="/leaderboard" element={<LeaderBoard></LeaderBoard>}></Route>
+			<Route
+				exact
+				path="/leaderboard"
+				element={<LeaderBoard></LeaderBoard>}
+			></Route>
 		</Routes>
 	);
-}
- 
+};
+
 export default AllRoutes;
